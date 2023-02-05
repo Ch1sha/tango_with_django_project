b4eed846f9ba3602d107a6f32c4832ce8ebb7e9b
Ch1sha <2700038s@student.gla.ac.uk>
2023-02-05 14:47:41 +0000

    Completed Chapter 5
    All tests completed successfully

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)#this will be the primary key
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self): 
+        return self.name
+
+class Page(models.Model):#1 Category to many Pages relationship
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #foreign key
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
