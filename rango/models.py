09a4feb3c88c029567cf2ce78d635bf82b92a3c7
Ch1sha <2700038s@student.gla.ac.uk>
2023-02-07 22:37:47 +0000

    Completed Chapter 9
    All tests ran successfully

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)#this will be the primary key
@@ -24,3 +26,12 @@ class Page(models.Model):#1 Category to many Pages relationship
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+# This line is required. Links UserProfile to a User model instance (1 to 1 relationship)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    def __str__(self):
+        return self.user.username
